aaf1b78a7b4ff447a4786aac0c11b28be757bab8
Neil Patil <npat692@aucklanduni.ac.nz>
2020-07-26 21:56:59 +1200

    changing

---
+++ my-app/src/App.tsx
@@ -0,0 +1,38 @@
+import React,{useState} from 'react';
+import './App.css';
+import SearchBar from './Components/SearchBarComponents/SearchBar';
+import MediaGrid from './Components/MediaGridComponent/MediaGrid';
+import { IUserInput } from './Common/Interfaces';
+import {createMuiTheme, MuiThemeProvider} from '@material-ui/core';
+
+const theme = createMuiTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 700,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+})
+function App() {
+  const [UserInput, setUserInput] = useState<IUserInput>({
+    SearchQuery: "Batman"
+  });
+  function SetUserInput(a: IUserInput) {
+    setUserInput(a);    
+  }
+
+  return (
+    <div className="App" >
+      <h2>IMDB Movie Search</h2>
+      <MuiThemeProvider theme={theme}>
+          <SearchBar SetUserInput={(a: IUserInput) => SetUserInput(a)}/>
+          <MediaGrid SearchQuery={UserInput.SearchQuery}/>
+      </MuiThemeProvider>
+    </div>
+  );
+}
+
+export default App;
